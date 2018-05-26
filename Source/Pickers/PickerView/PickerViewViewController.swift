0c8e4624ba7bbdb29db05c90060523e2a1be96a4
Carlos Crane <hapticappsdev@gmail.com>
2018-05-26 13:01:53 -0500

    Update PickerViewViewController.swift

--- Source/Pickers/PickerView/PickerViewViewController.swift
+++ Source/Pickers/PickerView/PickerViewViewController.swift
@@ -1,74 +1,79 @@
 import UIKit
 
 extension UIAlertController {
-    
+
     /// Add a picker view
     ///
     /// - Parameters:
     ///   - values: values for picker view
     ///   - initialSelection: initial selection of picker view
     ///   - action: action for selected value of picker view
-    public func addPickerView(values: PickerViewViewController.Values,  initialSelection: PickerViewViewController.Index? = nil, action: PickerViewViewController.Action?) {
+    public func addPickerView(values: PickerViewViewController.Values,  initialSelection: [PickerViewViewController.Index]? = nil, action: PickerViewViewController.Action?) {
         let pickerView = PickerViewViewController(values: values, initialSelection: initialSelection, action: action)
         set(vc: pickerView, height: 216)
     }
 }
 
 final public class PickerViewViewController: UIViewController {
-    
+
     public typealias Values = [[String]]
     public typealias Index = (column: Int, row: Int)
     public typealias Action = (_ vc: UIViewController, _ picker: UIPickerView, _ index: Index, _ values: Values) -> ()
-    
+
     fileprivate var action: Action?
     fileprivate var values: Values = [[]]
-    fileprivate var initialSelection: Index?
-    
+    fileprivate var initialSelection: [Index]?
+
     fileprivate lazy var pickerView: UIPickerView = {
         return $0
     }(UIPickerView())
-    
-    public init(values: Values, initialSelection: Index? = nil, action: Action?) {
+
+    public init(values: Values, initialSelection: [Index]? = nil, action: Action?) {
         super.init(nibName: nil, bundle: nil)
         self.values = values
         self.initialSelection = initialSelection
         self.action = action
     }
-    
+
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     deinit {
         Log("has deinitialized")
     }
-    
+
     override public func loadView() {
         view = pickerView
     }
-    
+
     override public func viewDidLoad() {
         super.viewDidLoad()
         pickerView.dataSource = self
         pickerView.delegate = self
     }
-    
+
     override public func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
-        if let initialSelection = initialSelection, values.count > initialSelection.column, values[initialSelection.column].count > initialSelection.row {
-            pickerView.selectRow(initialSelection.row, inComponent: initialSelection.column, animated: true)
+        if let initialSelection = initialSelection {
+            for index in initialSelection {
+                if (values.count > index.column && values[index.column].count > index.row) {
+                    pickerView.selectRow(index.row, inComponent: index.column, animated: true)
+                }
+            }
         }
+
     }
 }
 
 extension PickerViewViewController: UIPickerViewDataSource, UIPickerViewDelegate {
-    
+
     // returns the number of 'columns' to display.
     public func numberOfComponents(in pickerView: UIPickerView) -> Int {
         return values.count
     }
-    
-    
+
+
     // returns the # of rows in each component..
     public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
         return values[component].count
@@ -83,7 +88,7 @@ extension PickerViewViewController: UIPickerViewDataSource, UIPickerViewDelegate
      
      }
      */
-    
+
     // these methods return either a plain NSString, a NSAttributedString, or a view (e.g UILabel) to display the row for the component.
     // for the view versions, we cache any hidden and thus unused views and pass them back for reuse.
     // If you return back a different object, the old one will be released. the view will be centered in the row rect
@@ -104,4 +109,3 @@ extension PickerViewViewController: UIPickerViewDataSource, UIPickerViewDelegate
         action?(self, pickerView, Index(column: component, row: row), values)
     }
 }
-
