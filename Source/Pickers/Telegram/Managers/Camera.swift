6f27b5ca659616fddb0f8e80d75ebf4ca074f335
Carlos Crane <hapticappsdev@gmail.com>
2019-01-15 19:32:08 -0600

    Update Camera.swift

--- Source/Pickers/Telegram/Managers/Camera.swift
+++ Source/Pickers/Telegram/Managers/Camera.swift
@@ -125,15 +125,26 @@ public final class Camera {
         }
         
         private static func createQueue() -> DispatchQueue {
-            return DispatchQueue.init(label: "CameraQueue \(arc4random())",
-                                      qos: .background,
-                                      attributes: [],
-                                      autoreleaseFrequency: .workItem,
-                                      target: nil)
+            if #available(iOS 10.0, *) {
+                return DispatchQueue.init(label: "CameraQueue \(arc4random())",
+                    qos: .background,
+                    attributes: [],
+                    autoreleaseFrequency: .workItem,
+                    target: nil)
+            } else {
+                return DispatchQueue.init(label: "CameraQueue \(arc4random())",
+                    qos: .background,
+                    attributes: [],
+                    target: nil)
+            }
         }
         
         private static func createDevice() -> AVCaptureDevice? {
-            return AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .back)
+            if #available(iOS 10.0, *) {
+                return AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .back)
+            } else {
+                return nil
+            }
         }
         
         public static func == (lhs: PreviewStream, rhs: PreviewStream) -> Bool {
